a57db971a46ecb2db5f10311b1b7c486b7b78a93
BT_1N3_04@bktech.local <BT_1N3_04@bktech.local>
2018-09-18 09:16:37 -0400

    Initial commit

---
+++ src/DYang/DYangLib.java
@@ -0,0 +1,8 @@
+package DYang;
+
+public class DYangLib {
+    public static void println(String str)
+    {
+        System.out.println(str);
+    }
+}
---
+++ src/DYang/TestFile.java
@@ -0,0 +1,11 @@
+package DYang;
+
+import static DYang.DYangLib.println;
+
+public class TestFile {
+
+    public static void main(String[] args)
+    {
+        println("I love a cup of java chip in the morning.");
+    }
+}
